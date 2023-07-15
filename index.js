9842e131e66061099fa38fef7d32dd4a6c46be20
Hira <Hira@Hiras-MacBook-Pro.local>
2023-07-15 14:34:56 +0600

    necessary packages are installed

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+const port = 3020;
+
+app.use(express.json);
+
+app.get("/", (req, res) => {
+  res.send;
+});
+app.get("/userInfo", (req, res) => {
+  res.send;
+});
+app.get("/skills", (req, res) => {
+  res.send;
+});
+app.get("/identityInfo", (req, res) => {
+  res.send;
+});
+app.get("/contactInfo", (req, res) => {
+  res.send;
+});
